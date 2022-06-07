93c5f588f059bb81d8c32105e07ab304f54a3174
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-07 20:38:32 +0800

    Update figure2.m

--- CCN Final Project/IRS-relaying/figure2.m
+++ CCN Final Project/IRS-relaying/figure2.m
@@ -1,35 +1,36 @@
 %
-% This Matlab script generates Figure 2 in the paper:
-%   Intelligent Reflecting Surface vs. Decode-and-Forward: How Large Surfaces 
-%   Are Needed to Beat Relaying?, IEEE Wireless Communications Letters
+% Figure 2 - Typical channel gains as a function of the distance, when 
+%            including the antenna gains Gt = Gr = 5 dBi.
+% ref. E. Björnson, Ö. Özdogan and E. G. Larsson, "Intelligent Reflecting Surface 
+%      Versus Decode-and-Forward: How Large Surfaces are Needed to Beat Relaying?," 
+%      in IEEE Wireless Communications Letters, vol.9, no.2, pp.244-248, Feb.2020
 %
-close all;
+% close all;
 clear;
 clc;
 %
-% Set parameter values
+% set parameter values
 %
-% Carrier frequency (in GHz)
+% carrier frequency f_c = 3GHz
 %
-fc = 3;
+fc = 3; 
 %
-% Distances (in meter)
+% distances in meter
 %
 d = 10:0.1:100;
 %
-% Define the antenna gains at the transmitter and receiver
+% define the antenna gains at the transmitter and receiver
 %
-antennaGainTdBi = 5;
-antennaGainRdBi = 5;
+Gt = 5; % Tx antenna Gain in dBi
+Gr = 5; % Rx antenna Gain in dBi
 %
-% Compute channel gains based on the 3GPP Urban Micro in "Further
-% advancements for E-UTRA physical layer aspects (Release 9)."
-% 3GPP TS 36.814, Mar. 2010. Note that the antenna gains are included.
+% compute channel gains based on the 3GPP Urban Micro
+% Note that the antenna gains are included.
 %
-beta_3GPP_LOS  = db2pow(antennaGainTdBi + antennaGainRdBi - 28.0 - 20 * log10(fc) - 22.0 * log10(d));
-beta_3GPP_NLOS = db2pow(antennaGainTdBi + antennaGainRdBi - 22.7 - 26 * log10(fc) - 36.7 * log10(d));
+beta_3GPP_LOS  = db2pow(Gt + Gr - 28.0 - 20 * log10(fc) - 22.0 * log10(d));
+beta_3GPP_NLOS = db2pow(Gt + Gr - 22.7 - 26 * log10(fc) - 36.7 * log10(d));
 %
-% Plot simulation results
+% plot simulation results
 %
 figure;
 hold on; box on; grid on;
@@ -37,12 +38,13 @@ hold on; box on; grid on;
 plot(d, 10 * log10(beta_3GPP_LOS), 'k-.', 'LineWidth', 2);
 plot(d, 10 * log10(beta_3GPP_NLOS), 'r--', 'LineWidth', 2);
 %
-xlabel('Distance $d$ [m]', 'Interpreter', 'Latex');
-ylabel('Channel gain $\beta(d)$ [dB]', 'Interpreter', 'Latex');
+title('Figure 2');
+xlabel('Distance d [m]');
+ylabel('Channel gain \beta(d) [dB]');
 %
-legend({'UMi-LOS', 'UMi-NLOS'}, 'Interpreter', 'Latex');
-set(gca, 'fontsize', 18);
+legend({'UMi-LOS', 'UMi-NLOS'});
+set(gca, 'fontsize', 13);
+xlim([10 100]);
 ylim([-110 -50]);
 %
 %
-
